73cabc947226ff4a30662fcc03de716d9dd0bffa
pedro-serrano-camblor <p.serranocamblor@gmail.com>
2025-04-24 09:01:31 +0000

    añadida funcionalidad de contar likes usando JS, html y css

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// Tengo que avanzar más en los ejercicios de JS, ya que hay muchas cosas que aún no se hacer por mi mismo. Mañana intentaré avanzar un poco con esto
+// Por el momento me voy a centrar en añadir una funcionalidad más sencilla (añadir un botón de like a cada post), y dejo lo de ampliar la imagen en popup para cuando me sienta más cómodo con JS
+document.addEventListener("DOMContentLoaded", () => {
+
+    const botonesLike = document.querySelectorAll(".boton-likes");
+
+    for (let i = 0; i < botonesLike.length; i++) {
+
+        const boton = botonesLike[i];
+
+        boton.addEventListener("click", () => {
+
+            const contenedorLikes = boton.parentElement;
+            const contador = contenedorLikes.querySelector(".contador-likes");
+            let numeroLikes = parseInt(contador.textContent);
+            numeroLikes++;
+            contador.textContent = numeroLikes;
+            
+        });
+    }
+});
